e1fa6c0f17d57e014e4d4933c5b903b213eb7eff
Danilo Camargo Ferreira Rocha <danilo.rocha@meta.com.br>
2024-06-02 11:32:14 -0300

    clean cache

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,42 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    filename: 'main.js',
-    publicPath: '/'
+    // RESOLVE QUESTÃO DE CACHE DO PROJETO
+    filename: './[name].[contenthash].js'
   },
   mode: "development",
   devServer: {
     historyApiFallback: true,
-    port: 3000
+    port: 3000,
+    hot: true
   },
   module: {
     rules: [
       {
         test: /\.js$/,
-        use: 'babel-loader'
+        use: 'babel-loader',
+        include: path.resolve(__dirname, 'src')
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        // SEPARA ARQUIVO CSS
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
       }
     ]
   },
   plugins: [
+    // REMOVE ARQUIVOS DESNECESSÁRIOS NO DIST
+    new CleanWebpackPlugin(),
+    // SEPARA ARQUIVO CSS
+    new MiniCssExtractPlugin({
+      filename: './styles.[contenthash].css'
+    }),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'public/index.html'),
       filename: 'index.html'
